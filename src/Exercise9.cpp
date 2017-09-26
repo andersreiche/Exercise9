e4eb04bbad8e05445405f0be27d61ea64cbfe645
Anders Reiche <andersreiche@gmail.com>
2017-09-26 13:52:24 +0200

    program working with templates, only semantic errors

--- src/Exercise9.cpp
+++ src/Exercise9.cpp
@@ -7,22 +7,64 @@
 //=============================================================================
 
 #include <iostream>
+#include <assert.h>
 using namespace std;
 
-template <class T>
-T sum(T a, T b)
-{
-  T result;
-  result = a + b;
-  return result;
+int factorial(int n);
+
+// summing function
+template<class T>
+T sum_n(T x, int n) {
+	T a = 0;;
+	T b = 0;
+
+
+	// calculate x^n
+	if (n > 0) {
+		a = 1;	// if a = 0 everything returns 0;
+		for (int i = 0; i <= n; i++) {
+			a *= x;
+		}
+	}
+
+	// calculate n!
+	b = factorial(n);
+
+	return a / b;
 }
+
+// exp(x) = sum_n (x^n/n!)
+template<class T>
+T expfak(T x) {
+	T result = 0;
+
+	// loop the summing function x times
+	for (int n = 0; n <= x; n++) {
+		result += sum_n(x, n);
+	}
+
+	return result;
+}
+
 int main() {
-	  int i=5, j=6, k;
-	  double f=2.0, g=1, h;
-	  k=sum<int>(i,j);
-	  h=sum<double>(f,g);
-	  cout << k << '\n';
-	  cout << h << '\n';
+	cout << expfak(1) << '\n';
+	cout << expfak(2) << '\n';
+	cout << expfak(3) << '\n';
+	cout << expfak(4) << '\n';
+	cout << expfak(5) << '\n';
+	assert(expfak(0) == 0);
+	assert(expfak(1) == 2);
+	assert(expfak(3) == 5);
+	assert(expfak(4) == 8);
+	assert(expfak(5) == 11);
 
 	return 0;
+}
+
+// factorial function
+int factorial(int n) {
+	if (n > 1)
+		return n * factorial(n - 1);
+	else
+		return 1;
 }
