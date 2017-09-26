bef15e105548a7a11a398c9c408b669e26280c67
Anders Reiche <andersreiche@gmail.com>
2017-09-26 17:41:38 +0200

    did some more work, still fails final assertion, even though all sub functions are working

--- src/Exercise9.cpp
+++ src/Exercise9.cpp
@@ -8,32 +8,19 @@
 
 #include <iostream>
 #include <assert.h>
+#include <cmath>
 using namespace std;
 
 int factorial(int n);
+void test(void);
 
-// summing function
+// calculates factorial and power and divides them
 template<class T>
 T sum_n(T x, int n) {
-	T a = 0;;
-	T b = 0;
-
-
-	// calculate x^n
-	if (n > 0) {
-		a = 1;	// if a = 0 everything returns 0;
-		for (int i = 0; i <= n; i++) {
-			a *= x;
-		}
-	}
-
-	// calculate n!
-	b = factorial(n);
-
-	return a / b;
+	return pow(x, n) / factorial(n);
 }
 
-// exp(x) = sum_n (x^n/n!)
+//
 template<class T>
 T expfak(T x) {
 	T result = 0;
@@ -47,16 +34,12 @@ T expfak(T x) {
 }
 
 int main() {
-	cout << expfak(1) << '\n';
-	cout << expfak(2) << '\n';
-	cout << expfak(3) << '\n';
-	cout << expfak(4) << '\n';
-	cout << expfak(5) << '\n';
-	assert(expfak(0) == 0);
-	assert(expfak(1) == 2);
-	assert(expfak(3) == 5);
-	assert(expfak(4) == 8);
-	assert(expfak(5) == 11);
+	cout << expfak<int>(1) << '\n';
+	cout << expfak<int>(2) << '\n';
+	cout << expfak<int>(3) << '\n';
+	cout << expfak<int>(4) << '\n';
+	cout << expfak<int>(5) << '\n';
+	test();
 
 	return 0;
 }
@@ -67,4 +50,28 @@ int factorial(int n) {
 		return n * factorial(n - 1);
 	else
 		return 1;
+}
+
+// runs the algorithm against the target output results
+void test(void) {
+	assert(factorial(0) == 1);
+	assert(factorial(1) == 1);
+	assert(factorial(2) == 2);
+	assert(factorial(3) == 6);
+	assert(factorial(4) == 24);
+	assert(factorial(5) == 120);
+
+	assert(pow(2, 0) == 1);
+	assert(pow(2, 1) == 2);
+	assert(pow(2, 2) == 4);
+	assert(pow(2, 3) == 8);
+	assert(pow(2, 4) == 16);
+	assert(pow(2, 5) == 32);
+
+	assert(expfak<int>(0) == 0);
+	assert(expfak<int>(1) == 2);
+	assert(expfak<int>(3) == 5);
+	assert(expfak<int>(4) == 8);
+	assert(expfak<int>(5) == 11);
+
 }
